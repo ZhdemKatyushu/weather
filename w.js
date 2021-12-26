79722445db6efe1066bef590431dd46a8a1d8eee
Katerina <shiroi.maks@gmail.com>
2021-12-26 19:23:56 +0200

    Adding all files

---
+++ w.js
@@ -0,0 +1,32 @@
+// function greeting () {
+//     console.log('Hello, World');
+// }
+
+// let greeting = function() {
+//     console.log('Hello World');
+// }
+
+// let greeting = () => {
+//     console.log('Hello World');
+// }
+
+// let greeting = () => console.log('Hello World');
+
+// let sum = function(a, b, c) {
+//     return a + b + c;
+// }
+
+// let sum = (a, b, c) => {
+//     return a + b + c;
+// }
+
+let sum = (a, b, c) => a + b + c;
+
+console.log(sum(1,3,4));
+
+let square = function(x) {
+    return x * x;
+}
+
+let square = (x) => x * x;
+
